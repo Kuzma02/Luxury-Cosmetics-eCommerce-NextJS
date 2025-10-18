65ff6edb371ad08ac3f547684008a94c6accc8d9
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2025-10-18 10:56:57 +0200

    added bg overlay with tailwindcss

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,9 +1,12 @@
 import Button from "@/components/Button";
+import Image from "next/image";
 
 const Banner = () => {
     return (
-        <div className="banner h-[900px] flex flex-col justify-end max-sm:h-[700px] max-[460px]:h-[550px]">
-            <div className="px-10 py-8 flex flex-col gap-6 max-sm:gap-3 max-[400px]:px-5">
+        <div className="banner h-[900px] flex flex-col justify-end max-sm:h-[600px] max-[460px]:h-[550px] relative">
+            <Image src="/main banner.jpg" alt="Banner Image" fill className="object-cover absolute z-[-1]" />
+             <div className="absolute inset-0 bg-black/30" aria-hidden="true" />
+            <div className="px-10 py-8 flex flex-col gap-6 max-sm:gap-3 max-[400px]:px-5 z-10">
                 <h2 className="text-primary text-5xl leading-[50px] max-sm:text-4xl max-[460px]:text-3xl max-[360px]:text-2xl">
                     Elevate Your Beauty: <br />
                     Discover the Essence of Premium Elegance
